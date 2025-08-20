eff5805572ba964714d15aae657593cc79d0fc3d
siddhant-k08 <kulkarni.sid0809@gmail.com>
2025-08-21 01:53:01 +0530

    refactor

---
+++ src/config.ts
@@ -0,0 +1,90 @@
+export const siteConfig = {
+  name: "Siddhant Kulkarni",
+  title: "Fullstack Blockchain Developer",
+  description: "Portfolio website of Siddhant Kulkarni",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "kulkarni.sid0809@gmail.com",
+    linkedin: "https://linkedin.com/in/yourprofile",
+    twitter: "https://x.com/Siddhant8903",
+    github: "https://github.com/siddhant-k08",
+  },
+  aboutMe:
+    "I am a fullstack blockchain developer with a passion for building scalable and efficient blockchain applications. I am currently pursuing my M.Sc. in Blockchain Technology and I am looking for work in the blockchain space. I am also a part of BuidlGuidl where I contribute to The Graph <> BuidlGuidl Cohort",
+  skills: ["Ethereum", "Solana", "HTML", "CSS", "JavaScript", "TypeScript", "Solidity", "C", "C++", "Python",  "React", "Next", "Node.js", "GraphQL", "Ethers.js", "Web3.js", "The Graph", "Scaffold-ETH-2", "IPFS"],
+  projects: [
+    {
+      name: "Over-collateralized Lending Protocol",
+      description:
+        "Advanced DeFi lending protocol with ETH deposits and CORN token loans. Features 120% collateralization, liquidation mechanisms, and 10% liquidator fees for maintaining protocol security.",
+      link: "https://over-collateralized-lending.vercel.app/",
+      skills: ["Solidity", "TypeScript", "Ethereum"],
+    },
+    {
+      name: "Prediction Market",
+      description:
+        "",
+      link: "",
+      skills: ["Solidity", "TypeScript", "Ethereum"],
+    },
+    {
+      name: "Decentralized Exchange",
+      description:
+        "Trade ERC20 BALLOONS ($BAL) with ETH in a decentralized galaxy of tokens. Connect your wallet, check your assets, and swap seamlessly in a UI built for the future.",
+      link: "https://balloon-dex-nextjs.vercel.app/",
+      skills: ["Solidity", "TypeScript", "Ethereum"],
+    },
+    {
+      name: "Dice Game",
+      description:
+        "Provably fair blockchain dice game with dynamic prize pools. Players roll dice to win prizes, with 40% of entry fees going to prizes and 60% funding future rewards.",
+      link: "https://dice-game-chi-neon.vercel.app/",
+      skills: ["Solidity", "TypeScript", "Ethereum"],
+    },
+    {
+      name: "NFT DAO",
+      description:
+        "Complete DAO ecosystem with NFT collection, marketplace, and governance. Enables on-chain voting, proposal creation, and collective NFT purchasing decisions.",
+      link: "https://nft-dao-nine.vercel.app/",
+      skills: ["Solidity", "TypeScript", "Ethereum"],
+    }
+  ],
+  experience: [
+    {
+      company: "BuidlGuidl",
+      title: "Web3 Developer",
+      dateRange: "June 2024 - Present",
+      bullets: [
+        "Member of The Graph <> BuidlGuidl Cohort’s Pilot Program, which comprises of technical content creation like blog posts, READMEs, etc.", 
+        "Assisted in a few technical projects regarding Scaffold-ETH 2 Subgraph package and create-eth subgraph extension."
+      ],
+    },
+    {
+      company: "The Graph Advocates",
+      title: "Web3 Advocate",
+      dateRange: "Nov 2023 - Oct 2024",
+      bullets: [
+        "Part of The Graph Advocates Program which includes, technical content writing, community care, and public speaking.",
+        "Created blogs on subgraph deployment and building full-stack dApps using toolkits like Scaffold-ETH-2 and The Graph."
+      ],
+    }
+  ],
+  education: [
+    {
+      school: "MIT World Peace University",
+      degree: "Masters of Science in Blockchain Technology",
+      dateRange: "2024 - 2026",
+      achievements: [
+        ""
+      ],
+    },
+    {
+      school: "Savitribai Phule Pune University",
+      degree: "Bachelor of Computer Applications",
+      dateRange: "2021 - 2024",
+      achievements: [
+        ""
+      ],
+    },
+  ],
+};
